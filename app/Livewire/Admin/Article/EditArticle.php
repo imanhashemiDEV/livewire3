f20aeaa1b7fe9a24cb2ac8dea08df072caad0ae1
iman hashemi <hashemi.iman@gmail.com>
2024-03-02 13:17:46 +0330

    edit article

--- app/Livewire/Admin/Article/EditArticle.php
+++ app/Livewire/Admin/Article/EditArticle.php
@@ -2,14 +2,67 @@
 
 namespace App\Livewire\Admin\Article;
 
+use App\Models\Article;
+use App\Models\Category;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EditArticle extends Component
 {
+    use WithFileUploads;
+
+    #[Rule('required')]
+    public $title;
+    #[Rule('required')]
+    public $category_id;
+    #[Rule('required')]
+    public $body;
+    public $image;
+    public $article;
+
+    public function mount(Article $article)
+    {
+      $this->article = $article;
+      $this->title = $article->title;
+      $this->category_id = $article->category_id;
+      $this->body = $article->body;
+    }
+
+    #[On('setBody')]
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+    public function updateArticle()
+    {
+        $this->validate();
+
+        if($this->image){
+            $name = time().'.'.$this->image->getClientOriginalExtension();
+            $this->image->storeAs('photos/articles',$name,'public');
+        }
+
+
+        $this->article->update([
+            'title'=>$this->title,
+            'body'=>$this->body,
+            'category_id'=>$this->category_id,
+            'image'=>$this->image ? $name : $this->article->image,
+        ]);
+
+        $this->reset('title', 'body','image');
+        session()->flash('message','مقاله ویرایش شد');
+        $this->redirectRoute('articles');
+
+    }
+
     #[Layout('admin.master')]
     public function render()
     {
-        return view('livewire.admin.article.edit-article');
+        $categories = Category::query()->pluck('title','id');
+        return view('livewire.admin.article.edit-article',compact('categories'));
     }
 }
