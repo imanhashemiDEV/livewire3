d81c03ae2f6c0660e0f77fb9b6ebc63ff03f4257
iman hashemi <hashemi.iman@gmail.com>
2024-03-06 18:51:08 +0330

    Locked Properties

--- routes/admin.php
+++ routes/admin.php
@@ -7,5 +7,6 @@ Route::get('/users', \App\Livewire\Admin\User\UserList::class)->name('users');
 Route::get('/categories', \App\Livewire\Admin\Category\Categories::class)->name('categories');
 Route::get('/articles', \App\Livewire\Admin\Article\Articles::class)->name('articles');
 Route::get('/create_article', \App\Livewire\Admin\Article\CreateArticle::class)->name('create.article');
-Route::get('/edit_article/{article}', \App\Livewire\Admin\Article\EditArticle::class)->name('edit.article');
+//Route::get('/edit_article/{article}', \App\Livewire\Admin\Article\EditArticle::class)->name('edit.article');
+Route::get('/edit_article/{id}', \App\Livewire\Admin\Article\EditArticle::class)->name('edit.article');
 Route::post('upload_ckeditor_image',[\App\Http\Controllers\Admin\GalleryController::class,'ckeditor_image'])->name('ckeditor.upload');
